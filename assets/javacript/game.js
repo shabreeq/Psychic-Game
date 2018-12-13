b3e39b92d7fb6e817dc174a594623fe7435f3ea8
ShaBree Quinones <shabreeq@gmail.com>
2018-12-13 17:24:50 -0500

    created js structure

---
+++ assets/javacript/game.js
@@ -0,0 +1,18 @@
+<script>
+
+{/* create variables */}
+
+
+
+{/* // When guess is correct the wins count will go up by 1*/}
+
+
+{/* // After 9 incorrect guesses, the losses count will go up by 1*/}
+
+
+
+{/* // After each guess the guesses left will go down by 1 */}
+
+{/* // After each guess it will display  */}
+
+</script>
