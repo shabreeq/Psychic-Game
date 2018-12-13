1b8673e3808abf0b3bf9c974a633e57d5c473696
ShaBree Quinones <shabreeq@gmail.com>
2018-12-13 18:31:40 -0500

    started js code

--- assets/javacript/game.js
+++ assets/javacript/game.js
@@ -1,18 +1,11 @@
-<script>
 
-{/* create variables */}
-
-
-
-{/* // When guess is correct the wins count will go up by 1*/}
-
-
-{/* // After 9 incorrect guesses, the losses count will go up by 1*/}
+  var computerChoices = 0;
+  var winCount = 0;
+  var lossCount = 0;
+  var guessCount = 7;
+  var letterGuessed = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'
+];
 
 
 
-{/* // After each guess the guesses left will go down by 1 */}
 
-{/* // After each guess it will display  */}
-
-</script>
