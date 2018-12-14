46fc11034d4d5e7702d0f1dfa9d6947313396005
ShaBree Quinones <shabreeq@gmail.com>
2018-12-14 12:37:08 -0500

    saving

--- assets/javacript/game.js
+++ assets/javacript/game.js
@@ -6,6 +6,5 @@
   var letterGuessed = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'
 ];
 
-
-
-
+var user = document.getElementById("computerChoices");
+}
