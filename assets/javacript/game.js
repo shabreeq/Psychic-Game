eb93a91a9db317437de5f983dbafa464e1862875
ShaBree Quinones <shabreeq@gmail.com>
2019-01-21 12:00:29 -0500

    saving work

--- assets/javacript/game.js
+++ assets/javacript/game.js
@@ -1,33 +1,52 @@
 
-  var computerGuess = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var computerChoices = ['a', 'b', 'c'];
 
-  var winCount = 0;
-  var lossCount = 0;
-  var guessCount = 7;
-  var letterGuessed = 0;
+  var win = 0;
+  var loss = 0;
+  var guesses = 7;
+  var guessed = 0;
 
-var user = document.getElementById("wins");
-document.onkeyup = function(event){
-  var userGuess = event.key;
 
-  var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+  var winCount = document.getElementById("wins");
+  var lossCount = document.getElementById("losses");
+  var guessCount = document.getElementById("guessesLeft");
+  var letterGuessed = document.getElementById("guessesSoFar");
 
-  console.log("User guess: " + userGuess);
-  console.log("Computer guess: " + computerGuess);
+  // var user = document.getElementById("wins");
 
-  for (var i = 0; i < computerGuess.length[i]; i++);{
+  document.onkeyup = function(event){
 
-  if(userGuess === computerGuess){
-    wins++
-    }
-  if(userGuess !== computerGuess){
-      losses++
-      }
+    var userGuess = event.key;
 
-   
-
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
   
+  
+    // for (var i = 0; i < winCount.length; i++);{
+    for (var i = 0; i < guessCount.length; i++);{
+  
+    if((userGuess === computerGuess)){
+     
+      win++
+      guesses-- 
+      console.log("User guess: " + userGuess);
+      console.log("Computer guess: " + computerGuess);
+
+    }
+    if((userGuess !== computerGuess)){
+     
+      loss++
+      guesses--
+      console.log("User guess: " + userGuess);
+      console.log("Computer guess: " + computerGuess);
+
+    }
+    if((guessCount <= 0)){
+      
+    }
   
   }
 
-}
+  }
+  
+
+ 
