80b841ca2b5e9806fa19942693d09f706c6bd3b1
ShaBree Quinones <shabreeq@gmail.com>
2019-01-21 00:03:00 -0500

    saving work

--- assets/javacript/game.js
+++ assets/javacript/game.js
@@ -1,10 +1,33 @@
 
-  var computerChoices = 0;
+  var computerGuess = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
+
   var winCount = 0;
   var lossCount = 0;
   var guessCount = 7;
-  var letterGuessed = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'
-];
+  var letterGuessed = 0;
 
-var user = document.getElementById("computerChoices");
+var user = document.getElementById("wins");
+document.onkeyup = function(event){
+  var userGuess = event.key;
+
+  var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+  console.log("User guess: " + userGuess);
+  console.log("Computer guess: " + computerGuess);
+
+  for (var i = 0; i < computerGuess.length[i]; i++);{
+
+  if(userGuess === computerGuess){
+    wins++
+    }
+  if(userGuess !== computerGuess){
+      losses++
+      }
+
+   
+
+  
+  
+  }
+
 }
