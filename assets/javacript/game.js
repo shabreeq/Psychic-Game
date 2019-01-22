a93b19938175beb66340ccbf86d205eef2e227e3
ShaBree Quinones <shabreeq@gmail.com>
2019-01-22 10:45:28 -0500

    saving work

--- assets/javacript/game.js
+++ assets/javacript/game.js
@@ -1,5 +1,5 @@
 
-  var computerChoices = ['a', 'b', 'c'];
+  var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
 
   var win = 0;
   var loss = 0;
